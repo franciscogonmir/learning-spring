7e7e70e99e1d637478c0546e6ed482257e2eaff4
franciscogonzalezMiranda <franciscogonmirdeveloper@gmail.com>
2022-02-18 17:48:16 +0100

    refactor DateUtil class

--- src/main/java/com/fjgonmir/lil/learningspring/utils/DateUtil.java
+++ src/main/java/com/fjgonmir/lil/learningspring/utils/DateUtil.java
@@ -1,23 +1,23 @@
 package com.fjgonmir.lil.learningspring.utils;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtil {
 
-    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private DateUtil() {
+        throw new IllegalStateException("Utility class");
+    }
+
 
     public static Date createDateFromDateString(String dateString) {
-        Date date = null;
         try {
-            date = (dateString != null) ? DATE_FORMAT.parse(dateString) : new Date();
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            return (dateString != null) ? format.parse(dateString) : new Date();
         } catch (ParseException e) {
             e.printStackTrace();
-        } finally {
-            return date;
         }
-
+        return null;
     }
 }
