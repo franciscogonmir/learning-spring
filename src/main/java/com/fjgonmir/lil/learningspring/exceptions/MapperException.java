148c23576b830c061e706459a4d007e5bdf8d5be
franciscogonzalezMiranda <franciscogonmirdeveloper@gmail.com>
2022-02-18 17:16:12 +0100

    refactor mapperException class

--- src/main/java/com/fjgonmir/lil/learningspring/exceptions/MapperException.java
+++ src/main/java/com/fjgonmir/lil/learningspring/exceptions/MapperException.java
@@ -2,9 +2,8 @@ package com.fjgonmir.lil.learningspring.exceptions;
 
 public class MapperException extends Exception {
 
-    private String message;
-
-    public MapperException(String message){
-        this.message = message;
+    public MapperException(String message) {
+        super(message);
     }
+
 }
