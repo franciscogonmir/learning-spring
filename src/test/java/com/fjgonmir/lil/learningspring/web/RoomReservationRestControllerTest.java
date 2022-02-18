caff3ea4bad160c032dd134da7a039eb81048efd
franciscogonzalezMiranda <franciscogonmirdeveloper@gmail.com>
2022-02-18 14:27:21 +0100

    add RoomReservationRestController test

---
+++ src/test/java/com/fjgonmir/lil/learningspring/web/RoomReservationRestControllerTest.java
@@ -0,0 +1,27 @@
+package com.fjgonmir.lil.learningspring.web;
+
+import com.fjgonmir.lil.learningspring.Business.services.ReservationService;
+import com.fjgonmir.lil.learningspring.controller.web.RoomReservationRestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RoomReservationRestController.class)
+class RoomReservationRestControllerTest {
+
+    @MockBean
+    private ReservationService roomReservationService;
+
+    private MockMvc mockMvc;
+
+    @Test
+    void getRoomReservationsReturn200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("api/reservations"))
+                .andExpect(status().isOk());
+    }
+}
